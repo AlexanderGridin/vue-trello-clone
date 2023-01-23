e0ef17cf729c7ecc44ad61181d817bc9c193748b
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-23 13:40:01 +0200

    project initialized. structure changed. Board component created

---
+++ src/App/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Board from "./components/Board/BoardComponent.vue";
+
+const TITLE = "Board Title from script";
+</script>
+
+<template>
+  <h1 class="header">Hello Vue World</h1>
+  <Board :title="TITLE" />
+</template>
+
+<style scoped>
+.header {
+  border: 1px solid green;
+  padding: 7px;
+  text-align: center;
+  margin: 0;
+}
+</style>
