f38d28736d1a5f350b96f6c84ab53d2adc2e0147
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-27 18:00:54 +0200

    static data updated

--- src/static-data/board.ts
+++ src/static-data/board.ts
@@ -1,3 +1,5 @@
+import { TaskModel } from "@/App/components/Task/models/TaskModel";
+import { TasksListModel } from "@/App/components/TasksList/models/TasksListModel";
 import type { BoardModel } from "@pages/BoardPage/models/BoardModel";
 import { generateId } from "@shared/utils/generateId";
 
@@ -5,15 +7,15 @@ export const BOARD: BoardModel = {
   id: generateId(),
   title: "Test Board",
   lists: [
-    {
-      id: generateId(),
-      title: "TODO:",
-      tasks: [],
-    },
-    {
-      id: generateId(),
-      title: "DONE:",
-      tasks: [],
-    },
+    new TasksListModel({
+      title: "Todo",
+      tasks: [
+        new TaskModel({ listId: "2", content: "Test task 1" }),
+        new TaskModel({ listId: "2", content: "Test task 2" }),
+        new TaskModel({ listId: "2", content: "Test task 3" }),
+      ],
+    }),
+    new TasksListModel({ title: "In Progress" }),
+    new TasksListModel({ title: "Done" }),
   ],
 };
