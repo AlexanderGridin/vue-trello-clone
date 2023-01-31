a5c565fe1bb58cd1a6c34bdb45c18e925b0358b3
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-01-31 18:53:59 +0200

    AppLayout added

--- src/App/App.vue
+++ src/App/App.vue
@@ -1,7 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppLayout from "./components/AppLayout.vue";
+</script>
 
 <template>
-  <RouterView />
+  <AppLayout>
+    <template #sidebar>
+      <div>Sidebar</div>
+    </template>
+
+    <template #content>
+      <router-view />
+    </template>
+  </AppLayout>
 </template>
 
 <style scoped></style>
