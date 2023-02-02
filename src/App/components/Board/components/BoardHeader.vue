3f8fa80ae0a11d95f70ec3e1d15fb8053f1ac946
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-02 12:27:58 +0200

    Board component added

---
+++ src/App/components/Board/components/BoardHeader.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import IconButton from "@shared/components/IconButton.vue";
+import { MaterialIcon } from "@/shared/components/Icon/enums/MaterialIcon";
+import { ButtonStyle } from "@/shared/components/Button/enums/ButtonStyle";
+import type { BoardModel } from "@/App/pages/BoardPage/models/BoardModel";
+
+defineProps<{
+  board: BoardModel;
+}>();
+
+defineEmits<{
+  (e: "onRemove"): void;
+}>();
+</script>
+
+<template>
+  <div class="container">
+    <h2 class="title">{{ board.title }}</h2>
+
+    <IconButton
+      class="button"
+      :icon="MaterialIcon.Close"
+      :visual-style="ButtonStyle.Error"
+      @click="$emit('onRemove')"
+    />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+
+.title {
+  margin: 0;
+  font-size: 24px;
+  font-weight: bold;
+  margin-right: 7px;
+}
+
+.button {
+  margin-left: auto;
+}
+</style>
