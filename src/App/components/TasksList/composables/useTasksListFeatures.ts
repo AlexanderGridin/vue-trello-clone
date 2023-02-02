7dad49ea6d8503b4db4ee51fe37d2d13e77e1ec5
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-02 14:24:26 +0200

    TasksList updated

---
+++ src/App/components/TasksList/composables/useTasksListFeatures.ts
@@ -0,0 +1,12 @@
+import { useBoardPageState } from "@pages/BoardPage/state/useBoardPageState";
+import type { TaskModel } from "../../Task/models/TaskModel";
+import type { TasksListModel } from "../models/TasksListModel";
+
+export const useTasksListFeatures = () => {
+  const state = useBoardPageState();
+
+  const removeList = (list: TasksListModel) => state.removeList(list);
+  const removeTask = (task: TaskModel) => state.removeTask(task);
+
+  return { removeList, removeTask };
+};
