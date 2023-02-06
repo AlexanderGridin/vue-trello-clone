d3bca30af9a32fcbc82880a1157f898223c63410
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-06 15:34:41 +0200

    split AddItem and AddItemForm

--- src/App/components/AddItem/AddItem.vue
+++ src/App/components/AddItem/AddItem.vue
@@ -1,38 +1,14 @@
 <script setup lang="ts">
-import AddButton from "../AddButton/AddButton.vue";
 import { useAddItemFeatures } from "./composables/useAddItemFeatures";
-import AddItemForm from "./components/AddItemForm/AddItemForm.vue";
-import type { AddItemFormValue } from "./components/AddItemForm/models/AddItemFormValue";
 import { useAddItemState } from "./composables/useAddItemState";
 
-export interface AddItemProps {
-  buttonText: string;
-  placeholder?: string;
-  isDark?: boolean;
-}
-
-export interface AddItemEmit {
-  (e: "onAdd", entity: AddItemFormValue): void;
-}
-
-defineProps<AddItemProps>();
-const emit = defineEmits<AddItemEmit>();
-
 const state = useAddItemState();
-const { hideForm, showForm, addItem } = useAddItemFeatures(state, emit);
+const { hideForm, showForm } = useAddItemFeatures(state);
 </script>
 
 <template>
-  <AddButton v-if="!state.isShowForm" :is-text-dark="isDark" @click="showForm">
-    {{ buttonText }}
-  </AddButton>
-
-  <AddItemForm
-    v-if="state.isShowForm"
-    :placeholder="placeholder"
-    @on-submit="addItem"
-    @on-cancel="hideForm"
-  />
+  <slot name="button" v-bind="{ showForm, isShowForm: state.isShowForm }" />
+  <slot name="form" v-bind="{ hideForm, isShowForm: state.isShowForm }" />
 </template>
 
 <style scoped></style>
--- src/App/components/AddItem/composables/useAddItemFeatures.ts
+++ src/App/components/AddItem/composables/useAddItemFeatures.ts
@@ -1,19 +1,12 @@
 import type { Ref } from "vue";
-import type { AddItemEmit } from "../AddItem.vue";
-import type { AddItemFormValue } from "../components/AddItemForm/models/AddItemFormValue";
 import type { AddItemState } from "./useAddItemState";
 
-export const useAddItemFeatures = (
-  state: Ref<AddItemState>,
-  emit: AddItemEmit
-) => {
-  const showForm = () => (state.value.isShowForm = true);
-  const hideForm = () => (state.value.isShowForm = false);
-
-  const addItem = (entity: AddItemFormValue) => {
-    hideForm();
-    emit("onAdd", entity);
+export const useAddItemFeatures = (state: Ref<AddItemState>) => {
+  const showForm = () => {
+    state.value.isShowForm = true;
   };
 
-  return { showForm, hideForm, addItem };
+  const hideForm = () => (state.value.isShowForm = false);
+
+  return { showForm, hideForm };
 };
