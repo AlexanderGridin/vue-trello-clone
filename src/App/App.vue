87154120f3e5736822cbd4417b92ad91589fa3de
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-06 11:36:12 +0200

    App updated with AppSidebar

--- src/App/App.vue
+++ src/App/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import AppLayout from "./components/AppLayout.vue";
-import AppNavigation from "./components/AppNavigation/AppNavigation.vue";
+import AppLayout from "./components/AppLayout/AppLayout.vue";
+import AppSidebar from "@app/components/AppSidebar/AppSidebar.vue";
 </script>
 
 <template>
   <AppLayout>
     <template #sidebar>
-      <AppNavigation />
+      <AppSidebar />
     </template>
 
     <template #content>
