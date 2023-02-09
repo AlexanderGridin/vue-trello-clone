69a17241da499a150d220c469d01dfbadf5b0fba
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-02-09 10:44:19 +0200

    added boards moving

--- src/App/pages/BoardsPage/state/composables/useBoardsPageStateActions.ts
+++ src/App/pages/BoardsPage/state/composables/useBoardsPageStateActions.ts
@@ -1,5 +1,8 @@
 import type { BoardsPageState } from "../models/BoardsPageState";
 import type { BoardModel } from "@app/entities/Board/BoardModel";
+import type { ArrayUtilConfigWithArrayItem } from "@shared/utils/array/models/ArrayUtilConfigWithArrayItem";
+import { moveItemAfterArrayItem } from "@shared/utils/array/moveItemAfterArrayItem";
+import { moveItemBeforeArrayItem } from "@shared/utils/array/moveItemBeforeArrayItem";
 
 export const useBoardsPageStateActions = (state: BoardsPageState) => {
   const setBoards = (boards: BoardModel[]) => (state.value.boards = boards);
@@ -22,5 +25,35 @@ export const useBoardsPageStateActions = (state: BoardsPageState) => {
 
   const setIsLoaded = (isLoaded: boolean) => (state.value.isLoaded = isLoaded);
 
-  return { addBoard, removeBoard, setBoards, setIsLoaded, updateBoard };
+  const moveBoard = (boardToMove: BoardModel, boardToReplace: BoardModel) => {
+    const boardToMoveIndex = state.value.boards.findIndex(
+      ({ id }: BoardModel) => id === boardToMove.id
+    );
+    const boardToReplaceIndex = state.value.boards.findIndex(
+      ({ id }: BoardModel) => id === boardToReplace.id
+    );
+
+    const movingConfig: ArrayUtilConfigWithArrayItem<BoardModel> = {
+      array: state.value.boards,
+      item: boardToMove,
+      arrayItem: boardToReplace,
+      uniqueKey: "id",
+    };
+
+    const updatedBoards =
+      boardToMoveIndex < boardToReplaceIndex
+        ? moveItemAfterArrayItem(movingConfig)
+        : moveItemBeforeArrayItem(movingConfig);
+
+    state.value.boards = updatedBoards;
+  };
+
+  return {
+    addBoard,
+    removeBoard,
+    setBoards,
+    setIsLoaded,
+    updateBoard,
+    moveBoard,
+  };
 };
